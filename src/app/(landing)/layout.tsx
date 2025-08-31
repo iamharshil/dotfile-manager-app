92d1a110ff000e6d3c291f6dba3098475eab5d20
harshil <harshilc71@gmail.com>
2025-08-31 12:29:38 +0530

    NEW: added nextjs-loader

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<ThemeProvider attribute="class" defaultTheme="system" disableTransitionOnChange>
+					<NextTopLoader />
 					{children}
 				</ThemeProvider>
 			</body>
